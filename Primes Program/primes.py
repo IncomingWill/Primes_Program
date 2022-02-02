2cf3b4fe01e98fe42166660cc116cbd389946ea6
IncomingWill <thewilliamedwards@gmail.com>
2022-02-01 19:57:25 -0600

    Add project files.

---
+++ Primes Program/primes.py
@@ -0,0 +1,8 @@
+# Title: Primes Program
+# Program created by William Schaeffer
+# CPS 313
+# P. 428-429, Exercise 12, Primes Program
+# 02.01.22
+
+# This program will allow the user to enter a number. The program will display a list of all intgeters up to that number
+    # and determine if each is a prime or composite number
